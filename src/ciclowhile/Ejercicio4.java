9e7c50dde32cd2692990256da7d43106fb610d86
leotorresval <leotorresval92@gmail.com>
2024-09-19 15:11:54 -0500

    Ejercicio5

--- src/ciclowhile/Ejercicio4.java
+++ src/ciclowhile/Ejercicio4.java
@@ -3,12 +3,23 @@ package ciclowhile;
 import java.util.Scanner;
 
 public class Ejercicio4 {
+
     public static void main(String[] args) {
         //Programa que solicite 10 numeros y clasifique
         //en positivos y negativos
         //Al final imprimir cuantos positivos y negativos
         //Se ingresaron
-        int c=0,cp=0,cn=0;
+        int c = 0, cp = 0, numero = 0;
         Scanner teclado = new Scanner(System.in);
+        while (c < 10) {
+            System.out.println("["+(c+1)+"] Ingrese un numero");
+            numero = teclado.nextInt();
+            if (numero >= 0) {
+                cp++;
+            }
+            c++;
+        }
+        System.out.println("Se ingresaron " + cp + " numeros positivos");
+        System.out.println("Se ingresaron " + (c - cp) + " numeros negativos");
     }
 }
---
+++ src/ciclowhile/Ejercicio5.java
@@ -0,0 +1,8 @@
+package ciclowhile;
+
+public class Ejercicio5 {
+    //Acumular los números enteros ingresados por teclado, 
+    //detenerse cuando el acumulador sobrepase el valor de 20, 
+    //luego de terminado el ciclo imprimir el acumulador
+
+}
