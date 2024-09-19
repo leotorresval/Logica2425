d3710c3d1505a6dc9a12b08406ed498b46eda9d5
leotorresval <leotorresval92@gmail.com>
2024-09-19 11:52:05 -0500

    Ejercicio4

--- src/ciclowhile/Ejercicio4.java
+++ src/ciclowhile/Ejercicio4.java
@@ -1,11 +1,14 @@
 package ciclowhile;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
         //Programa que solicite 10 numeros y clasifique
         //en positivos y negativos
         //Al final imprimir cuantos positivos y negativos
         //Se ingresaron
-        
+        int c=0,cp=0,cn=0;
+        Scanner teclado = new Scanner(System.in);
     }
 }
