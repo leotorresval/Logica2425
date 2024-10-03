e6c19f7fccf3ae5eee1963f6e4e09bb3effab22a
leotorresval <leotorresval92@gmail.com>
2024-10-02 22:23:18 -0500

    Uso de metodos

---
+++ src/metodos/Metodos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos;
+
+/**
+ *
+ * @author User
+ */
+public class Metodos {
+    public static void main(String[] args) {
+        
+    }
+    
+    //METODOS SIN RETORNO Y SIN PARAMETROS
+    public static void saludo(){
+        
+    }
+    //METODOS SIN RETORNO Y CON PARAMETROS
+    public static void saludo(String nombre){
+        
+    }
+    //METODOS CON RETORNO Y SIN PARAMETROS
+    public static String retornaSaludo(){
+        return "";
+    }
+    //METODOS CON RETORNO Y CON PARAMETROS
+    public static String retornaSaludo(String nombre){
+        return "";
+    }
+}
